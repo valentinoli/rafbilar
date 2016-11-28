af56e275968b9e3d7fee09102918e6d2b5f5a332
valentinoli <valentin081194@gmail.com>
2016-11-28 22:17:47 +0000

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,9 @@ const pgp = require('pg-promise')();
 
 const router = express.Router();
 
-function unknown(str) {
-  const h = str.match(/.{1,4}/g) || [];
-  let back = '';
-  for (let j = 0; j < h.length; j++) {
-    back += String.fromCharCode(parseInt(h[j], 16));
-  }
-  return back;
-}
-
-const p = unknown('0070006f007300740067007200650073');
-
 const ENV = process.env.DATABASE_URL;
 
-const db = pgp(ENV || `postgres://${p}:${p}@localhost:5432/postgres`);
+const db = pgp(ENV || `postgres://postgres:postgres@localhost:5432/postgres`);
 
 // Route þegar notandi kemur á forsíðu
 router.get('/', (req, res, next) => {
